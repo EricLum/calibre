21915021c1741b1f2bd169256ab386b89a2b1a37
eal6gf <elum1234@gmail.com>
2018-01-19 16:59:00 -0500

    need to fix table structure

--- db/migrate/20180119205338_create_media.rb
+++ db/migrate/20180119205338_create_media.rb
@@ -2,8 +2,10 @@ class CreateMedia < ActiveRecord::Migration[5.1]
   def change
     create_table :media do |t|
       t.string :title
-      t.string :source
-      t.integer :playlist_id
+      t.string :artist
+      t.string :type
+      t.integer :like_count
+      t.string :filesource
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+5.times do
+  User.create(username: Faker::Name.unique.name)
+end
+
+5.times do
+  Playlist.create(name: Faker::String.unique.name, User.sample())
+end
+
+5.times do
+  Medium.create(title: Faker::Music.unique.name, )
+end
